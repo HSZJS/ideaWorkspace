dbe0f1d9c265ad01fd3acb6e738c65813f0a75cd
git clone git@101.200.180.22:/home/git/tvsinfo/tvsinfo_spider.git <xhdhjava@foxmail.com>
2017-03-08 16:27:22 +0800

    java测试文档
    1.算法实现
    2.新方法的实现

---
+++ TestToolWorkspace/yuesefuhuan/src/yuesefuhuan/yuesefuhuan.java
@@ -0,0 +1,11 @@
+package yuesefuhuan;
+
+/**
+ * Created by heshuzhan on 2017/3/8.
+ */
+public class yuesefuhuan
+{
+    public static void main(String args[]){
+        System.out.println("hello yuesefu");
+    }
+}
